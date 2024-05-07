3afd1ff37aea46066567c36060c9d7cf0c4d30cf
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-05-07 19:35:05 -0300

    Login/Bienvenida Update

--- app/src/main/java/com/example/kotlin_parcial1/Login.kt
+++ app/src/main/java/com/example/kotlin_parcial1/Login.kt
@@ -1,2 +1,23 @@
 package com.example.kotlin_parcial1
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_parcial1.ui.theme.Kotlin_Parcial1Theme
+
+@Composable
+fun Login(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    Kotlin_Parcial1Theme {
+        Login("Android")
+    }
+}
--- app/src/main/java/com/example/kotlin_parcial1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_parcial1/MainActivity.kt
@@ -22,25 +22,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Login("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kotlin_Parcial1Theme {
-        Greeting("Android")
-    }
-}
