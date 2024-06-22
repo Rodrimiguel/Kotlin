22e287436b4d5b0c1e9719f08554f1ef16c1d4c3
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-22 18:52:39 -0300

    Armando nuevamente estructura Composable/Eliminada previamente por error

--- app/src/main/java/com/example/kotlin_parcial1/presentacion/login/LoginPage.kt
+++ app/src/main/java/com/example/kotlin_parcial1/presentacion/login/LoginPage.kt
@@ -1,4 +1,29 @@
 package com.example.kotlin_parcial1.presentacion.login
 
-class LoginPage {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_parcial1.ui.theme.Kotlin_Parcial1Theme
+
+fun LoginPage(
+
+) {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPacePreview (){
+    Kotlin_Parcial1Theme {
+        LoginPage()
+    }
 }
