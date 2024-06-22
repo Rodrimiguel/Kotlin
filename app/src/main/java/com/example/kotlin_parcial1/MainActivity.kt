f52c4e25d2313c87f82a4cc81f56a5f6c4d66a2e
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-22 12:48:03 -0300

    Reestructuracion de codigos y paquetes

--- app/src/main/java/com/example/kotlin_parcial1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_parcial1/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_parcial1.presentacion.MainPage
 import com.example.kotlin_parcial1.ui.theme.Kotlin_Parcial1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Login("Android")
+                    MainPage()
                 }
             }
         }
---
+++ app/src/main/java/com/example/kotlin_parcial1/presentacion/MainPage.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin_parcial1.presentacion
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_parcial1.ui.theme.Kotlin_Parcial1Theme
+
+@Composable
+fun MainPage (modifier: Modifier = Modifier) {
+
+}
+
+
+
+@Preview (showBackground = true)
+@Composable
+fun MainPagePreview(){
+    Kotlin_Parcial1Theme {
+        MainPage()
+    }
+}
