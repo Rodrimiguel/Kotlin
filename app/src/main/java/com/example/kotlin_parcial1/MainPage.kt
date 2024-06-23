78684f461d744517afb2590c098a9e3c5fbb7ca9
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-06-23 01:47:38 -0300

    ProblemaSolucionado/ && Dependencias/Librerias

--- app/src/main/java/com/example/kotlin_parcial1/MainPage.kt
+++ app/src/main/java/com/example/kotlin_parcial1/MainPage.kt
@@ -3,7 +3,8 @@ package com.example.kotlin_parcial1
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.kotlin_parcial1.ui.theme.Kotlin_Parcial1Theme
 
@@ -13,6 +14,15 @@ fun MainPage(modifier: Modifier = Modifier) {
     val navHostController = rememberNavController()
 
     MainNavHost(modifier, navHostController)
+}
+
+@Composable
+fun MainNavHost(
+    modifier: Modifier = Modifier,
+    navHostController : NavHostController
+){
+
+
 }
 
 
